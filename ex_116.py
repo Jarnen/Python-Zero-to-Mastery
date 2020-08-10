251dd509792de8d64004629c2cd6fe56f878fc16
Jarnen Richard <richard.jarnen@gmail.com>
2020-08-10 13:14:15 +0800

    refactor: completed *args use

--- ex_116.py
+++ ex_116.py
@@ -1,14 +1,15 @@
 class Cat:
     species = 'mammal'
+
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
 
 # Instantiate the Cat object with 3 cats
-peanut = Cat("Ropo", 1)
-garfield = Cat("Toppo", 5)
-snickers = Cat("Tapi", 1)
+ropo = Cat("Ropo", 1)
+toppo = Cat("Toppo", 5)
+grace = Cat("Grace", 1)
 
 
 # Find the oldest cat
@@ -17,4 +18,5 @@ def get_oldest_cat(*args):
 
 
 # Output
-print(f"The oldest cat is {get_oldest_cat(peanut.age, garfield.age, snickers.age)} years old.")
+print(
+    f"The oldest cat is {get_oldest_cat(ropo.age, toppo.age, grace.age)} years old.")
